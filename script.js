76aa6a3c833a4ee1fdd752b10766b81d2927dcbc
CodeXii <78667460+codeXii@users.noreply.github.com>
2023-10-19 06:34:18 -0700

    Update script.js

--- script.js
+++ script.js
@@ -5,11 +5,14 @@ var cpassword = document.querySelector('.cpassword');
 
 
 redirect.addEventListener('click', function(){
+
     if(password.value != cpassword.value){
         alert('Passwords do not match');
+    }else if(password.value == "" || cpassword.value == ""){
+        alert("Please fill in a valid password");
     }else{
-        alert("Registration in progress");
-         window.location.href = "./success.html";
+        alert('Registration in progress | Creating account');
+        window.location.href = "./success.html";
     }
     
 });
